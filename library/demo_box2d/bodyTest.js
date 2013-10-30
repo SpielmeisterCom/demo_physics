2acc833a2d0a2672c47e6eaabd0e3f16d7fdb03f
Julian Haupt <jh@spielmeister.com>
2013-10-30 15:46:51 +0100

    copied initial demo_box2d project

---
+++ library/demo_box2d/bodyTest.js
@@ -0,0 +1,133 @@
+/**
+ * @class demo_box2d.bodyTest
+ * @singleton
+ */
+
+define(
+	'demo_box2d/bodyTest',
+	[
+		'spell/functions'
+	],
+	function(
+		_
+	) {
+		'use strict'
+
+
+		var removeCrate = function( entityManager ) {
+			var components = entityManager.getComponentMapById( 'spell.component.physics.body' )
+
+			if( _.size( components ) === 0 ) return
+
+			var id = _.find(
+				_.keys( components ),
+				function( id ) {
+					var entityMetaData = entityManager.getComponentById( id, 'spell.component.entityMetaData' )
+
+					return entityMetaData.name === ''
+				}
+			)
+
+			if( !id ) return
+
+			entityManager.removeEntity( id )
+		}
+
+		/**
+		 * Creates an instance of the system.
+		 *
+		 * @constructor
+		 * @param {Object} [spell] The spell object.
+		 */
+		var BodyTest = function( spell ) {}
+
+		BodyTest.prototype = {
+			/**
+		 	 * Gets called when the system is created.
+		 	 *
+		 	 * @param {Object} [spell] The spell object.
+			 */
+			init: function( spell ) {
+
+			},
+
+			/**
+		 	 * Gets called when the system is destroyed.
+		 	 *
+		 	 * @param {Object} [spell] The spell object.
+			 */
+			destroy: function( spell ) {
+
+			},
+
+			/**
+		 	 * Gets called when the system is activated.
+		 	 *
+		 	 * @param {Object} [spell] The spell object.
+			 */
+			activate: function( spell ) {
+				// registering for specific input events with inputManager.addListener
+				var processKeyDown = this.processKeyDown = function( event ) {
+					var keyCode = event.keyCode
+					
+					if( keyCode === spell.inputManager.KEY.RIGHT_ARROW ) {
+						removeCrate( spell.entityManager )
+						
+					} else if( keyCode === spell.inputManager.KEY.V ) {
+						spell.audioContext.play( spell.assetManager.get( 'sound:demo_box2d.voiceSample' ).resource )
+					}
+				}
+
+				spell.inputManager.addListener( 'keyDown', processKeyDown )
+
+
+				var processPointerDown = this.processPointerDown = function( event ) {
+					var worldPosition = spell.renderingContext.transformScreenToWorld( event.position )
+
+					spell.entityManager.createEntity( {
+						entityTemplateId : 'demo_box2d.smallCrate',
+						config : {
+							"spell.component.2d.transform" : {
+								translation : worldPosition
+							}
+						}
+					} )
+				}
+
+				spell.inputManager.addListener( 'pointerDown', processPointerDown )
+			},
+
+			/**
+		 	 * Gets called when the system is deactivated.
+		 	 *
+		 	 * @param {Object} [spell] The spell object.
+			 */
+			deactivate: function( spell ) {
+				spell.inputManager.removeListener( 'keyDown', this.processKeyDown )
+				spell.inputManager.removeListener( 'pointerDown', this.processPointerDown )
+			},
+
+			/**
+		 	 * Gets called to trigger the processing of game state.
+		 	 *
+			 * @param {Object} [spell] The spell object.
+			 * @param {Object} [timeInMs] The current time in ms.
+			 * @param {Object} [deltaTimeInMs] The elapsed time in ms.
+			 */
+			process: function( spell, timeInMs, deltaTimeInMs ) {
+				var inputManager = spell.inputManager
+
+				// polling for a UP_ARROW key down on every process call
+				if( inputManager.isKeyPressed( inputManager.KEY.UP_ARROW ) ) {
+					var id = spell.entityManager.getEntityIdsByName( 'player' )[ 0 ]
+
+					if( id ) {
+						spell.box2dWorlds.main.applyForce( id, [ 0, 2500 ] )
+					}
+				}
+			}
+		}
+
+		return BodyTest
+	}
+)
---
+++ library/demo_box2d/scene/Scene1.js
@@ -0,0 +1,17 @@
+define(
+	'demo_box2d/scene/Scene1',
+	[
+		'spell/functions'
+	],
+	function(
+		_
+	) {
+		'use strict'
+
+
+		return {
+			init : function( spell, sceneConfig ) {},
+			destroy : function( spell, sceneConfig ) {}
+		}
+	}
+)
